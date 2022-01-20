0522d53b64156b80693be41bff868d98f6ec74b7
rowjay007 <rowlives@gmail.com>
2022-01-20 12:23:46 +0100

    creating classes and object literal in features

--- features/variable.js
+++ features/variable.js
@@ -1,4 +1,4 @@
-var apple = 5;
+var apple = 15;
 var speed = "fast";
 var hasName = true;
 var nothingMuch = null;
@@ -9,3 +9,15 @@ var now = new Date();
 var colors = ["red", "green", "blue"];
 var myNumbers = [1, 2, 3, 4];
 var truths = [true, true, false];
+//Classes
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
+var car = new Car();
+//Object Literal
+var point = {
+    x: 10,
+    y: 30
+};
--- features/variable.ts
+++ features/variable.ts
@@ -1,4 +1,4 @@
-const apple: number = 5;
+const apple: number = 15;
 const speed: string = "fast";
 const hasName: boolean = true;
 const nothingMuch: null = null;
@@ -13,5 +13,15 @@ let colors: string[] = ["red", "green", "blue"];
 let myNumbers: number[] = [1, 2, 3, 4];
 let truths: boolean[] = [true, true, false];
 
+//Classes
 
+class Car {}
 
+let car: Car = new Car();
+
+//Object Literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 30,
+};
