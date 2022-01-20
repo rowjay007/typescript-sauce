8f26a9f047b9e8a6c1662e9be8fd2c2bdac22b7e
rowjay007 <rowlives@gmail.com>
2022-01-20 12:37:34 +0100

    creating function and any type check

--- features/variable.js
+++ features/variable.js
@@ -21,3 +21,14 @@ var point = {
     x: 10,
     y: 30
 };
+// function
+var logNumber = function (i) {
+    console.log(i);
+};
+//When to use annotations
+// 1) Function that returns the "any" type
+//Any type is just as "string" or boolean are
+// Avoid variable with "any" at all costs
+var json = '{"x": 10, "y": 20}';
+var coordinates = JSON.parse(json);
+console.log(coordinates); //{"x": 10, "y": 20}
--- features/variable.ts
+++ features/variable.ts
@@ -25,3 +25,17 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 30,
 };
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+//When to use annotations
+// 1) Function that returns the "any" type
+//Any type is just as "string" or boolean are
+// Avoid variable with "any" at all costs
+
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); //{"x": 10, "y": 20}
