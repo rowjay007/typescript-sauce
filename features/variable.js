736f013a6a9da7ee801f01a694be152c5c075262
rowjay007 <rowlives@gmail.com>
2022-01-20 12:18:02 +0100

    adding array to features

--- features/variable.js
+++ features/variable.js
@@ -5,3 +5,7 @@ var nothingMuch = null;
 var nothing = undefined;
 //built in objects
 var now = new Date();
+//Arrays
+var colors = ["red", "green", "blue"];
+var myNumbers = [1, 2, 3, 4];
+var truths = [true, true, false];
--- features/variable.ts
+++ features/variable.ts
@@ -6,3 +6,12 @@ const nothing: undefined = undefined;
 
 //built in objects
 const now: Date = new Date();
+
+//Arrays
+
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3, 4];
+let truths: boolean[] = [true, true, false];
+
+
+
