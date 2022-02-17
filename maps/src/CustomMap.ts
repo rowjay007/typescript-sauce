d017d5883c9798f2dae9e0bd3aa07fede215ca0e
rowjay007 <rowlives@gmail.com>
2022-02-17 15:57:25 +0100

    refactoring useMarker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,13 @@
 import { Company } from "./Company";
 import { User } from "./User";
+
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -12,22 +20,22 @@ export class CustomMap {
       },
     });
   }
-  addUserMarker(user: User): void {
+  addUserMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  }
-}
+//    addCompanyMarker(company: Company): void {
+//     new google.maps.Marker({
+//       map: this.googleMap,
+//       position: {
+//         lat: company.location.lat,
+//         lng: company.location.lng,
+//       },
+//     });
+//   }
+// }
