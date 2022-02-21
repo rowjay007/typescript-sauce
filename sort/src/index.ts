3a8683b0ed3f7ecf1ae65c71fc96039a440eeab9
rowjay007 <rowlives@gmail.com>
2022-02-21 15:26:11 +0100

    sorting string with characters

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from './Sorter'
+import { Sorter } from "./Sorter";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+/* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(numbersCollection.data); */
+
+const charactersCollection = new CharactersCollection("Saerf");
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
