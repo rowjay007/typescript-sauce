16d964f8550ff50caeaee57f36dbc5433fe5e9fb
rowjay007 <rowlives@gmail.com>
2022-02-22 17:10:57 +0100

    refactoring with abstract classes

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,12 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-const CharactersCollection_1 = require("./CharactersCollection");
-/* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data); */
-const charactersCollection = new CharactersCollection_1.CharactersCollection("Saerf");
-const sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+/* const charactersCollection = new CharactersCollection("Saerf");
+charactersCollection.sort()
+console.log(charactersCollection.data); */
+/* const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
+ */ 
--- sort/src/LinkedList.ts
+++ sort/src/LinkedList.ts
@@ -1,11 +1,11 @@
-import console, { count } from "console";
+import { Sorter } from "./Sorter";
 
 class Node {
   next: Node | null = null;
   constructor(public data: number) {}
 }
 
-export class LinkedList {
+export class LinkedList extends Sorter{
   head: Node | null = null;
 
   add(data: number): void {
