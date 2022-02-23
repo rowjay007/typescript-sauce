61bb37003e6f584860662b97756fbf7167bb3aaa
rowjay007 <rowlives@gmail.com>
2022-02-23 12:34:18 +0100

    created CSV file reader and refactored with TypeScript enum

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,24 +1,22 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync("football.csv", {
-    encoding: "utf-8",
-})
-    .split("\n")
-    .map((row) => {
-    return row.split(",");
-});
-const MatchResult = {
-    awayWin: "A",
-    homeWin: "H",
-    draw: "D",
-};
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
+//enum -enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["awayWin"] = "A";
+    MatchResult["homeWin"] = "H";
+    MatchResult["draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+/* const MatchResult = {
+  awayWin: "A",
+  homeWin: "H",
+  draw: "D",
+}; */
 let manUnitedWins = 0;
-for (let match of matches) {
+for (let match of reader.data) {
     if (match[1] === "Man United" && match[5] == MatchResult.homeWin) {
         manUnitedWins++;
     }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,21 +1,21 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
-const MatchResult = {
+const reader = new CsvFileReader("football.csv");
+reader.read();
+//enum -enumeration
+enum MatchResult {
+  awayWin = "A",
+  homeWin = "H",
+  draw = "D",
+}
+/* const MatchResult = {
   awayWin: "A",
   homeWin: "H",
   draw: "D",
-};
+}; */
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] == MatchResult.homeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.awayWin) {
