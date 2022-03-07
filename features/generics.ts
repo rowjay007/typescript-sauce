9ece12e21f9c2a38a7ae3ff57351e95a628bce8a
rowjay007 <rowlives@gmail.com>
2022-03-07 11:24:46 +0100

    adding contraints to generics

--- features/generics.ts
+++ features/generics.ts
@@ -46,3 +46,30 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything<string>(["a", "b", "c"]);
+
+//Generic Constraints
+
+class Car {
+ print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<House>([new Car(), new Car()]);
