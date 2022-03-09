ebea7e4edcea3895da7ceec0c43f183c93a74774
rowjay007 <rowlives@gmail.com>
2022-03-09 16:19:23 +0100

    adding types and routes setup

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello, Rowjay");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send(`
+        <div>
+        <h1>Hi Rowjay </h1>
+        </div>
+        `);
+});
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, Rowjay");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    `
+        <div>
+        <h1>Hi Rowjay </h1>
+        </div>
+        `
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
