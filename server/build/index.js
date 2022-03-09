d25b951905a1e6bf96b65b64409e1a39addbf1e7
rowjay007 <rowlives@gmail.com>
2022-03-09 16:34:16 +0100

    creating loginRoutes

--- server/build/index.js
+++ server/build/index.js
@@ -4,14 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send(`
-        <div>
-        <h1>Hi Rowjay </h1>
-        </div>
-        `);
-});
+app.use(loginRoutes_1.router);
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
