946b74f56d8a2c3c3bf0b07091b77bbcb3c2d9b0
rowjay007 <rowlives@gmail.com>
2022-03-15 09:45:38 +0100

    adding custom function with decorator factory

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,14 +5,23 @@ class Boat {
     return `This boats color is ${this.color}`;
   }
 
-  @logError
+  @logError("Oops boat was sunk in ocean")
   pilot(): void {
-      throw new Error()
+    throw new Error();
     console.log("swish");
   }
 }
-
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  console.log("Target:", target);
-  console.log("Key:", key);
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
+
+new Boat().pilot();
